b8a5cc27d89deed5b8fff5e194e145e2ccad95c7
tiago <paezsantiago@gmail.com>
2017-09-15 19:45:05 -0300

    Add viewHolder pattern

--- app/src/main/java/com/codepath/tiago/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/tiago/flickster/adapters/MovieArrayAdapter.java
@@ -22,6 +22,13 @@ import java.util.List;
 
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
+    // View lookup cache
+    private static class ViewHolder {
+        TextView title;
+        TextView overview;
+        ImageView image;
+    }
+
     public MovieArrayAdapter(Context context, List<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
     }
@@ -29,26 +36,66 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Get the movie for this position.
         Movie movie = getItem(position);
 
+        // Check if an existing view is being reused, otherwise inflate the view.
+        ViewHolder viewHolder;
         if (convertView == null) {
+
+            // If there's no view to re-use, inflate a brand new view for row.
+            viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.overview = (TextView) convertView.findViewById(R.id.tvOverview);
+            viewHolder.image = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            
+            // Cache the viewHolder object inside the fresh view.
+            convertView.setTag(viewHolder);
+        } else {
+
+            // View is being recycled, retrieve the viewHolder object from tag.
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView ivMovie = (ImageView) convertView.findViewById(R.id.ivMovieImage);
-        // Clear image from convertview
-        ivMovie.setImageResource(0);
-
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
-
-        // Populate data.
-        tvTitle.setText(movie.getOriginalTitle());
-        tvOverview.setText(movie.getOverview());
-
-        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivMovie);
+        // Populate the data from the data object via the viewHolder object into the template view.
+        viewHolder.title.setText(movie.getOriginalTitle());
+        viewHolder.overview.setText(movie.getOverview());
+        viewHolder.image.setImageResource(0);
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.image);
 
         return convertView;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
